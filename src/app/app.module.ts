0dfb8662e3286ccd1821d1b495ce0fbe7566d31c
osmankabdulla <osmankabdulla@users.noreply.github.com>
2019-07-11 14:43:48 -0400

    remote server and basic UI
    added a remote server that works and a basic UI that is ugly for now, next step is to route the successful login to the my accounts page

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import{FormsModule,ReactiveFormsModule} from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SignInFormComponent } from './sign-in-form/sign-in-form.component';
+import { UserComponent } from './user/user.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignInFormComponent,
+    UserComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
